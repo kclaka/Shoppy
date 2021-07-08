e51bd2d069f3197ceeb986caae2945efcacd1b8c
Kenny <kennyigbechi@gmail.com>
2021-07-08 12:43:14 -0700

    implement delete all

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ datastore_client = datastore.Client()
 firebase_request_adapter = requests.Request()
 
 app = Flask(__name__)
+app.secret_key = 'fsdfsdfowe2323r'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def root():
     id_token = request.cookies.get("token")
-    error_message = None
-    claims = None
+    claims, error_message = auth()
     items = None
 
     if id_token:
@@ -51,6 +51,25 @@ def store_item(email, dt, name, qty):
     datastore_client.put(entity)
 
 
+@app.route('/delete/<int:item>/', methods=['POST'])
+def delete(item):
+    claims, error_message = auth()
+
+    if claims:
+        delete_item('User', claims['email'], 'visit', item, 'Item')
+    return redirect(url_for('root'))
+
+
+@app.route('/delete_all/', methods=['POST'])
+def delete_all():
+    claims, error_message = auth()
+    if claims:
+        items = fetch_times(claims['email'])
+        for item in items:
+            delete_item('User', claims['email'], 'visit', item.id, 'Shopping List')
+    return redirect(url_for('root'))
+
+
 def fetch_times(email):
     ancestor = datastore_client.key('User', email)
     query = datastore_client.query(kind='visit', ancestor=ancestor)
@@ -65,6 +84,31 @@ def delete_item(user, email, entity, item_id, element):
     key = datastore_client.key(user, email, entity, item_id)
     datastore_client.delete(key)
     flash('%s successfully deleted' % element, 'deleted')
+
+
+def auth():
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+
+    if id_token:
+        try:
+            claims = google.oauth2.id_token.verify_firebase_token(
+                id_token, firebase_request_adapter)
+        except ValueError as exc:
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
+            error_message = str(exc)
+
+    return claims, error_message
+
+
+@app.context_processor
+def utility_functions():
+    def print_in_console(message):
+        print(str(message))
+
+    return dict(mdebug=print_in_console)
 
 
 if __name__ == '__main__':
